50c5c6d92c101f62ed53a10fe79eab256e5fab02
Katerina Ilievska <katerina.ilievska@inteligenta.io>
2025-02-03 17:46:14 +0100

    added docker compose

---
+++ service1/src/test/java/com/example/service1/Controller1Test.java
@@ -0,0 +1,26 @@
+package com.example.service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class Controller1Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetUserById() throws Exception {
+        mockMvc.perform(get("/api"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Simple api service 1"));
+    }
+
+}
